91ac7aa94346f1a73c6e0067a80c96c1d849ca98
Airat Yusuff <airat.yusuff@bjss.com>
2024-10-11 09:25:08 +0100

    revert to starting code

--- stackmachine.go
+++ stackmachine.go
@@ -2,95 +2,11 @@ package main
 
 import (
 	"errors"
-	"strconv"
-	"strings"
 )
 
-const MAX_NUMBER_IN_STACK = 50000
-
-type Store struct {
-	stack    []int
-	commands []string
-}
-
-func (s *Store) appendToStack(num int) {
-	s.stack = append(s.stack, num)
-}
-
-func (s *Store) popFromStack(inverseIndex int) int {
-	return s.stack[len(s.stack)-inverseIndex]
-}
-
-func (s *Store) removeElementsFromStack(numberOfElements int) {
-	s.stack = s.stack[:len(s.stack)-numberOfElements]
-}
-
-func isNumberOutOfBounds(number int) bool {
-	return number < 0 || number > MAX_NUMBER_IN_STACK
-}
-
 func StackMachine(commands string) (int, error) {
-	if len(commands) == 0 {
-		return 0, errors.New("")
-	}
-
-	machineStore := Store{
-		stack:    []int{},
-		commands: strings.Split(commands, " "),
-	}
-
-	for _, command := range machineStore.commands {
-		switch command {
-		case "DUP":
-			if len(machineStore.stack) < 1 {
-				return 0, errors.New("not enough numbers in stack for duplicating")
-			}
-			dupLastNumInStack := machineStore.popFromStack(1)
-			machineStore.appendToStack(dupLastNumInStack)
-
-		case "+":
-			if len(machineStore.stack) < 2 {
-				return 0, errors.New("not enough numbers in stack for add")
-			}
-
-			lastNumInStack := machineStore.popFromStack(1)
-			secondLastNumInStack := machineStore.popFromStack(2)
-
-			numTotal := lastNumInStack + secondLastNumInStack
-
-			if isNumberOutOfBounds(numTotal) {
-				return 0, errors.New("stack integer overflow")
-			}
-
-			machineStore.removeElementsFromStack(2)
-			machineStore.appendToStack(numTotal)
-
-		default:
-			numberToAppendToStack, err := strconv.Atoi(command)
-			if err != nil {
-				return 0, errors.New("invalid command")
-			}
-
-			if isNumberOutOfBounds(numberToAppendToStack) {
-				return 0, errors.New("number not within accepted range")
-			}
-
-			machineStore.appendToStack(numberToAppendToStack)
-		}
-	}
-
-	result := machineStore.stack[0]
-	return result, nil
-
+	return 0, errors.New("")
 }
-
-// func isTotalWithinLimit(number int, secondnumber int) bool {
-// 	total := number + secondnumber
-// 	if total < 0 || total <= MAX_NUMBER_IN_STACK {
-// 		return true
-// 	}
-// 	return false
-// }
 
 func main() {
 	// main is unused - run using
--- stackmachine_test.go
+++ stackmachine_test.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
 
@@ -14,134 +14,60 @@ func TestStartsWithEmptyStack(t *testing.T) {
 }
 
 // Write your own TDD tests here as you develop
-func TestCommandShouldNotBeEmpty(t *testing.T) {
-	command := ""
-	_, err := StackMachine(command)
-
-	if err == nil {
-		t.Errorf("Expected error, but received %s", err)
-	}
-}
-
-func TestCommandExceedsMaxInteger(t *testing.T) {
-	command := "50001"
-	_, err := StackMachine(command)
-
-	if err == nil {
-		t.Errorf("Expected error, but received %s", err)
-	}
-}
-func TestCommandBelowMinInteger(t *testing.T) {
-	command := "-1 -5"
-	_, err := StackMachine(command)
-
-	if err == nil {
-		t.Errorf("Expected error, but received %s", err)
-	}
-}
-
-func TestCommandShouldCauseOverflow(t *testing.T) {
-	commands := []string{
-		"50000 DUP +",
-		"50000 1 +",
-	}
-
-	for _, command := range commands {
-		got, err := StackMachine(command)
-
-		if err == nil {
-			t.Errorf("Expected error, but received %s", err)
-		}
-		fmt.Printf("result: %d", got)
-	}
-}
-
-func TestCommandTooFewNumsToAdd(t *testing.T) {
-	commands := []string{
-		"1 +",
-		"23 34 + +",
-	}
-
-	for _, command := range commands {
-		got, err := StackMachine(command)
-
-		if err == nil {
-			t.Errorf("Expected error, but received %s", err)
-		}
-		fmt.Printf("result: %d", got)
-	}
-}
-
-func TestInvalidCommands(t *testing.T) {
-	commands := []string{
-		"TH!SISINVAL!D",
-		"123sample",
-		"12 3THIS",
-	}
-
-	for _, command := range commands {
-		_, err := StackMachine(command)
-
-		if err == nil {
-			t.Errorf("Invalid command")
-		}
-	}
-
-}
 
 /*
 All these tests must pass for completion
 */
-// func TestAcceptanceTests(t *testing.T) {
-// 	tests := []struct {
-// 		name        string
-// 		commands    string
-// 		expected    int
-// 		expectedErr error
-// 	}{
-// 		{name: "empty error", commands: "", expected: 0, expectedErr: errors.New("")},
-// 		{name: "add overflow", commands: "50000 DUP +", expected: 0, expectedErr: errors.New("")},
-// 		{name: "too few add", commands: "99 +", expected: 0, expectedErr: errors.New("")},
-// 		{name: "too few minus", commands: "99 -", expected: 0, expectedErr: errors.New("")},
-// 		{name: "too few multiply", commands: "99 *", expected: 0, expectedErr: errors.New("")},
-// 		{name: "empty stack", commands: "99 CLEAR", expected: 0, expectedErr: errors.New("")},
-// 		{name: "sum single value", commands: "99 SUM", expected: 99, expectedErr: nil},
-// 		{name: "sum empty", commands: "SUM", expected: 0, expectedErr: errors.New("")},
-// 		{name: "normal +*", commands: "5 6 + 2 *", expected: 22, expectedErr: nil},
-// 		{name: "clear too few", commands: "1 2 3 4 + CLEAR 12 +", expected: 0, expectedErr: errors.New("")},
-// 		{name: "normal after clear", commands: "1 CLEAR 2 3 +", expected: 5, expectedErr: nil},
-// 		{name: "single integer", commands: "9876", expected: 9876, expectedErr: nil},
-// 		{name: "invalid command", commands: "DOGBANANA", expected: 0, expectedErr: errors.New("")},
-// 		{name: "normal +-*", commands: "5 9 DUP + + 43 - 3 *", expected: 60, expectedErr: nil},
-// 		{name: "minus", commands: "2 5 -", expected: 3, expectedErr: nil},
-// 		{name: "underflow minus", commands: "5 2 -", expected: 0, expectedErr: errors.New("")},
-// 		{name: "at overflow limit", commands: "25000 DUP +", expected: 50000, expectedErr: nil},
-// 		{name: "at overflow limit single value", commands: "50000 0 +", expected: 50000, expectedErr: nil},
-// 		{name: "overflow plus", commands: "50000 1 +", expected: 0, expectedErr: errors.New("")},
-// 		{name: "overflow single value", commands: "50001", expected: 0, expectedErr: errors.New("")},
-// 		{name: "times zero at overflow limit", commands: "50000 0 *", expected: 0, expectedErr: nil},
-// 		{name: "too few at first", commands: "1 2 3 4 5 + + + + * 999", expected: 0, expectedErr: errors.New("")},
-// 		{name: "normal simple", commands: "1 2 - 99 +", expected: 100, expectedErr: nil},
-// 		{name: "at overflow minus to zero", commands: "50000 50000 -", expected: 0, expectedErr: nil},
-// 		{name: "clear empties stack", commands: "CLEAR", expected: 0, expectedErr: errors.New("")},
-// 		{name: "normal sum", commands: "3 4 3 5 5 1 1 1 SUM", expected: 23, expectedErr: nil},
-// 		{name: "sum after clear stack", commands: "3 4 3 5 CLEAR 5 1 1 1 SUM", expected: 8, expectedErr: nil},
-// 		{name: "sum then too few", commands: "3 4 3 5 5 1 1 1 SUM -", expected: 0, expectedErr: errors.New("")},
-// 		{name: "fibonacci", commands: "1 2 3 4 5 * * * *", expected: 120, expectedErr: nil},
-// 	}
+func TestAcceptanceTests(t *testing.T) {
+	tests := []struct {
+		name        string
+		commands    string
+		expected    int
+		expectedErr error
+	}{
+		{name: "empty error", commands: "", expected: 0, expectedErr: errors.New("")},
+		{name: "add overflow", commands: "50000 DUP +", expected: 0, expectedErr: errors.New("")},
+		{name: "too few add", commands: "99 +", expected: 0, expectedErr: errors.New("")},
+		{name: "too few minus", commands: "99 -", expected: 0, expectedErr: errors.New("")},
+		{name: "too few multiply", commands: "99 *", expected: 0, expectedErr: errors.New("")},
+		{name: "empty stack", commands: "99 CLEAR", expected: 0, expectedErr: errors.New("")},
+		{name: "sum single value", commands: "99 SUM", expected: 99, expectedErr: nil},
+		{name: "sum empty", commands: "SUM", expected: 0, expectedErr: errors.New("")},
+		{name: "normal +*", commands: "5 6 + 2 *", expected: 22, expectedErr: nil},
+		{name: "clear too few", commands: "1 2 3 4 + CLEAR 12 +", expected: 0, expectedErr: errors.New("")},
+		{name: "normal after clear", commands: "1 CLEAR 2 3 +", expected: 5, expectedErr: nil},
+		{name: "single integer", commands: "9876", expected: 9876, expectedErr: nil},
+		{name: "invalid command", commands: "DOGBANANA", expected: 0, expectedErr: errors.New("")},
+		{name: "normal +-*", commands: "5 9 DUP + + 43 - 3 *", expected: 60, expectedErr: nil},
+		{name: "minus", commands: "2 5 -", expected: 3, expectedErr: nil},
+		{name: "underflow minus", commands: "5 2 -", expected: 0, expectedErr: errors.New("")},
+		{name: "at overflow limit", commands: "25000 DUP +", expected: 50000, expectedErr: nil},
+		{name: "at overflow limit single value", commands: "50000 0 +", expected: 50000, expectedErr: nil},
+		{name: "overflow plus", commands: "50000 1 +", expected: 0, expectedErr: errors.New("")},
+		{name: "overflow single value", commands: "50001", expected: 0, expectedErr: errors.New("")},
+		{name: "times zero at overflow limit", commands: "50000 0 *", expected: 0, expectedErr: nil},
+		{name: "too few at first", commands: "1 2 3 4 5 + + + + * 999", expected: 0, expectedErr: errors.New("")},
+		{name: "normal simple", commands: "1 2 - 99 +", expected: 100, expectedErr: nil},
+		{name: "at overflow minus to zero", commands: "50000 50000 -", expected: 0, expectedErr: nil},
+		{name: "clear empties stack", commands: "CLEAR", expected: 0, expectedErr: errors.New("")},
+		{name: "normal sum", commands: "3 4 3 5 5 1 1 1 SUM", expected: 23, expectedErr: nil},
+		{name: "sum after clear stack", commands: "3 4 3 5 CLEAR 5 1 1 1 SUM", expected: 8, expectedErr: nil},
+		{name: "sum then too few", commands: "3 4 3 5 5 1 1 1 SUM -", expected: 0, expectedErr: errors.New("")},
+		{name: "fibonacci", commands: "1 2 3 4 5 * * * *", expected: 120, expectedErr: nil},
+	}
 
-// 	for _, test := range tests {
+	for _, test := range tests {
 
-// 		got, err := StackMachine(test.commands)
+		got, err := StackMachine(test.commands)
 
-// 		if test.expectedErr != nil {
-// 			if err == nil {
-// 				t.Errorf("%s (%s) Expected error, but received nil", test.name, test.commands)
-// 			} else if err.Error() != test.expectedErr.Error() {
-// 				t.Errorf("%s (%s) got error %v, want %v", test.name, test.commands, err, test.expectedErr)
-// 			}
-// 		} else if got != test.expected {
-// 			t.Errorf("%s (%s) got %v, want %v", test.name, test.commands, got, test.expected)
-// 		}
-// 	}
-// }
+		if test.expectedErr != nil {
+			if err == nil {
+				t.Errorf("%s (%s) Expected error, but received nil", test.name, test.commands)
+			} else if err.Error() != test.expectedErr.Error() {
+				t.Errorf("%s (%s) got error %v, want %v", test.name, test.commands, err, test.expectedErr)
+			}
+		} else if got != test.expected {
+			t.Errorf("%s (%s) got %v, want %v", test.name, test.commands, got, test.expected)
+		}
+	}
+}
